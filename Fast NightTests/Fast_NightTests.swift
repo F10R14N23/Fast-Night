8332698705cdfcca6d8d1d189369a9cf7292a2a2
Florian.Caplet <capletflorian@gmail.com>
2026-03-30 00:49:17 +0200

    Initial Commit

---
+++ Fast NightTests/Fast_NightTests.swift
@@ -0,0 +1,16 @@
+//
+//  Fast_NightTests.swift
+//  Fast NightTests
+//
+//  Created by Florian on 30/03/2026.
+//
+
+import Testing
+
+struct Fast_NightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
